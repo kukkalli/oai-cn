71166e5056678da7dbc2a952da8becd7f512e907
Hanif Kukkalli <kukkalli@outlook.com>
2022-11-18 13:58:58 +0100

    first commit

---
+++ build/hss/build/CMakeFiles/hss_s6a.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/hss/openair-cn/src/oai_hss/s6a/s6a_auth_info.c" "/home/hss/openair-cn/build/hss/build/CMakeFiles/hss_s6a.dir/home/hss/openair-cn/src/oai_hss/s6a/s6a_auth_info.c.o"
+  "/home/hss/openair-cn/src/oai_hss/s6a/s6a_common.c" "/home/hss/openair-cn/build/hss/build/CMakeFiles/hss_s6a.dir/home/hss/openair-cn/src/oai_hss/s6a/s6a_common.c.o"
+  "/home/hss/openair-cn/src/oai_hss/s6a/s6a_error.c" "/home/hss/openair-cn/build/hss/build/CMakeFiles/hss_s6a.dir/home/hss/openair-cn/src/oai_hss/s6a/s6a_error.c.o"
+  "/home/hss/openair-cn/src/oai_hss/s6a/s6a_fd.c" "/home/hss/openair-cn/build/hss/build/CMakeFiles/hss_s6a.dir/home/hss/openair-cn/src/oai_hss/s6a/s6a_fd.c.o"
+  "/home/hss/openair-cn/src/oai_hss/s6a/s6a_in_addr.c" "/home/hss/openair-cn/build/hss/build/CMakeFiles/hss_s6a.dir/home/hss/openair-cn/src/oai_hss/s6a/s6a_in_addr.c.o"
+  "/home/hss/openair-cn/src/oai_hss/s6a/s6a_peers.c" "/home/hss/openair-cn/build/hss/build/CMakeFiles/hss_s6a.dir/home/hss/openair-cn/src/oai_hss/s6a/s6a_peers.c.o"
+  "/home/hss/openair-cn/src/oai_hss/s6a/s6a_purge_ue.c" "/home/hss/openair-cn/build/hss/build/CMakeFiles/hss_s6a.dir/home/hss/openair-cn/src/oai_hss/s6a/s6a_purge_ue.c.o"
+  "/home/hss/openair-cn/src/oai_hss/s6a/s6a_subscription_data.c" "/home/hss/openair-cn/build/hss/build/CMakeFiles/hss_s6a.dir/home/hss/openair-cn/src/oai_hss/s6a/s6a_subscription_data.c.o"
+  "/home/hss/openair-cn/src/oai_hss/s6a/s6a_supported_features.c" "/home/hss/openair-cn/build/hss/build/CMakeFiles/hss_s6a.dir/home/hss/openair-cn/src/oai_hss/s6a/s6a_supported_features.c.o"
+  "/home/hss/openair-cn/src/oai_hss/s6a/s6a_up_loc.c" "/home/hss/openair-cn/build/hss/build/CMakeFiles/hss_s6a.dir/home/hss/openair-cn/src/oai_hss/s6a/s6a_up_loc.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DAEMONIZE=0"
+  "FREEDIAMETER_VERSION=120"
+  "PACKAGE_BUGREPORT=\"openaircn-user@lists.eurecom.fr\""
+  "PACKAGE_VERSION=\"Branch: develop Abrev. Hash: 724542d Date: Thu Jun 22 16:17:12 2017 +0200\""
+  "_GNU_SOURCE"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/hss/openair-cn/src/oai_hss/utils"
+  "/home/hss/openair-cn/src/oai_hss/db"
+  "/home/hss/openair-cn/src/oai_hss/auc"
+  "/home/hss/openair-cn/src/oai_hss/access_restriction"
+  "/usr/local/include"
+  "/home/hss/openair-cn/src/oai_hss"
+  "/home/hss/openair-cn/src/oai_hss/s6a"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
